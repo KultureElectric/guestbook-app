29e428c31e7758597a2bbb0f43933e289af4490a
ruben <rubenusshd@gmail.com>
2018-11-27 20:17:25 +0100

    tested connection and added test entry to dv

--- routes/guestbookRoutes.js
+++ routes/guestbookRoutes.js
@@ -1,7 +1,17 @@
+const mongoose = require("mongoose");
+
+const Entry = mongoose.model("entries");
+
 module.exports = app => {
-  app.get("/", (req, res) => {
-    res.send({
-      hi: "there"
+  app.get("/", async (req, res) => {
+    const testEntry = new Entry({
+      user: "ruben",
+      heading: "test",
+      body: "asdfasdf"
     });
+
+    await testEntry.save();
+
+    res.send("Entry sent");
   });
 };

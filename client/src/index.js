6af92c68168aeef7b0ed06cc27080e52f0c02250
ruben <rubenusshd@gmail.com>
2018-11-28 21:37:34 +0100

    added redux and react-redux exosystem and materialitecss

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,22 @@
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import reducer from "./reducers";
+
+const store = createStore(reducer, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
